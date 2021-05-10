264a16f3a17389b54d76e06524dbeff9b0d1dd1f
Michael <36463000+mtruong1999@users.noreply.github.com>
2021-05-10 12:05:40 -0600

    Create package __init__ files
    For model and preprocess packages. Also added .vscode files and jupyter notebook checkpoint directories to gitignore

---
+++ src/model/__init__.py
@@ -0,0 +1,6 @@
+'''
+This package contains all of the models for classifying
+mask vs no-mask. They should all have the same interface 
+and will be used for training on a dataset and saving the 
+trained model to models/ in the project root.
+'''
---
+++ src/preprocess/__init__.py
@@ -0,0 +1,5 @@
+#from process_data import *
+# This will allow us to import from preprocess package without having
+# to explicitly state which file in the preprocess package it is coming
+# from.
+
